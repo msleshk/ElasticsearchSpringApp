ebe3ae3ec4aec46bc417e718c077cbab83c6a762
Maksim Leshko <msleshko2003@gmail.com>
2024-10-19 18:50:33 +0300

    Add liquidbase db migration

--- src/main/java/com/example/ElasticsearchSpringApp/model/Product.java
+++ src/main/java/com/example/ElasticsearchSpringApp/model/Product.java
@@ -1,6 +1,11 @@
 package com.example.ElasticsearchSpringApp.model;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.Cascade;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "Products")
@@ -8,10 +13,25 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "product_id")
+    @Column(name = "id")
     private Integer id;
 
+    @Column(name = "name")
+    private String name;
 
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "active")
+    private Boolean active;
+
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @Column(name="start_date")
+    @Temporal(TemporalType.DATE)
+    private Date startDate;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<Sku> skuList;
 
     public void setId(Integer id) {
         this.id = id;
@@ -19,5 +39,45 @@ public class Product {
 
     public Integer getId() {
         return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public List<Sku> getSkuList() {
+        return skuList;
+    }
+
+    public void setSkuList(List<Sku> skuList) {
+        this.skuList = skuList;
     }
 }
--- src/main/java/com/example/ElasticsearchSpringApp/model/Sku.java
+++ src/main/java/com/example/ElasticsearchSpringApp/model/Sku.java
@@ -1,4 +1,67 @@
 package com.example.ElasticsearchSpringApp.model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "sku")
 public class Sku {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "color")
+    private String color;
+
+    @Column(name = "availability")
+    private Boolean availability;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    private Product product;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Boolean getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(Boolean availability) {
+        this.availability = availability;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
 }
