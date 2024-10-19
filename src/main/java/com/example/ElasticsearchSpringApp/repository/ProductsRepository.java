4e209c66345cb22c7e79961e527eef2c506f6d2e
Maksim Leshko <msleshko2003@gmail.com>
2024-10-19 20:13:15 +0300

    Add mock data in project and repositories for work with db

---
+++ src/main/java/com/example/ElasticsearchSpringApp/repository/ProductsRepository.java
@@ -0,0 +1,4 @@
+package com.example.ElasticsearchSpringApp.repository;
+
+public interface ProductsRepository {
+}
---
+++ src/main/java/com/example/ElasticsearchSpringApp/repository/SkusRepository.java
@@ -0,0 +1,4 @@
+package com.example.ElasticsearchSpringApp.repository;
+
+public class SkusRepository {
+}
